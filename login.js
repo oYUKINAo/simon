f65d17041fd777c5594cbfec6ed9d064b8378025
Yukina Soga <yksakb48@gmail.com>
2023-03-06 13:40:55 -0700

    added function comments on login.js

--- login.js
+++ login.js
@@ -3,3 +3,6 @@ function login() {
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "play.html";
 }
+//we did assign as id=name for input for username
+//store input value in localstorage as userName 
+//I think window allows to see the username from other page
